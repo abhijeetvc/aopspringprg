13686186b3da22ddbc1fa282323140c701e87c56
abhi <abhijeetvc7@gmail.com>
2021-03-30 15:31:00 +0530

    Spring AOP advices

--- src/main/java/com/spaop/aopspringprg/AopspringprgApplication.java
+++ src/main/java/com/spaop/aopspringprg/AopspringprgApplication.java
@@ -14,9 +14,8 @@ public class AopspringprgApplication {
 
 }
 
-
 // AOP Terminology:
 // a) Aspect: It is cross-cutting concern.
 // b) JoinPoint: It is a point that represents the execution of method.
-// c) Advice: Advice is an action taken by aspect. (After, Before, Around, AfterReturning, AfterThrowing)
+// c) Advice: Advice is an action taken by aspect. (After, Before, Around, AfterReturning, AfterThrowing).
 // d) PointCut: A scenario that matches the joinpoint.
--- src/main/java/com/spaop/aopspringprg/aop/AopClass.java
+++ src/main/java/com/spaop/aopspringprg/aop/AopClass.java
@@ -1,6 +1,7 @@
 package com.spaop.aopspringprg.aop;
 
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -14,8 +15,16 @@ public class AopClass {
 //        System.out.println("in aop class");
 //    }
 
-    @Before("execution (* com.spaop.aopspringprg.impl.MyImpl.checkAop())")
-    public void executeAop(){
-        System.out.println("in aop class");
+//    @Before("execution (* com.spaop.aopspringprg.impl.MyImpl.checkAop())")
+//    public void executeAop(){
+//        System.out.println("in aop class");
+//    }
+
+    @AfterReturning(pointcut = "execution (* com.spaop.aopspringprg.impl.MyImpl.checkAop())",
+    returning = "retValue")
+    public void checkAfterReturning(Object retValue){
+          String value=(String)retValue;
+        System.out.println("Returned value : "+value);
     }
+
 }
